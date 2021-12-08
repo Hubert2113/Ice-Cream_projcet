18c524c8667592787e1991174111a3ac0607200c
Kamil Przebieracz <dmjs23@gmail.com>
2021-12-08 23:55:00 +0100

    preview carousel customer reviews

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss';
+
+//===== import dla karuzeli opini ===========
+import Siema from 'siema';
+const mySiema = new Siema();
+document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+document.querySelector('.next').addEventListener('click', () => mySiema.next());
+
+//===========================================
