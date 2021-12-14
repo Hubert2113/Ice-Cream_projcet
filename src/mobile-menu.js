94fbd14044f030e7bc347a43bbe0431ba822c5b2
PiotrekHadziewicz <piotrek.hadziewicz@gmail.com>
2021-12-14 10:47:51 +0100

    modal

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -1,12 +1,13 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-mobile-menu-open]'),
-    closeModalBtn: document.querySelector('[data-mobile-menu-close]'),
     modal: document.querySelector('[data-mobile-menu]'),
   };
 
+  const modalClose = document.querySelectorAll('[data-mobile-menu-close]');
+  modalClose.forEach(addEventListener('click', toggleModal));
   refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
